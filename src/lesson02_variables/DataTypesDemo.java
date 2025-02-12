66a1b1d66e7b0782d8f9c03cddf6b3fc86af05d7
NguyenDucLoi05 <nguyenducloi1105@gmail.com>
2025-02-12 15:11:02 +0700

    Updated DataTypesDemo and RefernceTypesExample wiht full content

--- src/lesson02_variables/DataTypesDemo.java
+++ src/lesson02_variables/DataTypesDemo.java
@@ -1,4 +1,31 @@
 package lesson02_variables;
 
 public class DataTypesDemo {
+    public static void main(String[] args) {
+        //Kieu so nguyen
+        byte myByte = 100;
+        short myShort = 30000;
+        int myInt = 100000;
+        long myLong = 10000000000L;// Can them 'L' o cuoi vi la so lon
+
+        //Kieu so thuc
+        float myFloat = 5.75f;//Can them 'f' o cuoi vi mac dinh la double
+        double myDouble = 19.99;//Khong can hau to
+
+        //Ky tu
+        char myChar = 'A';//Dung dau nhay don
+
+        //Boolean
+        boolean myBoolean = true;
+
+        //In ra man hinh
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Int: " + myInt);
+        System.out.println("Long: " + myLong);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Double: " + myDouble);
+        System.out.println("char: " + myChar);
+        System.out.println("Boolean: " + myBoolean);
+    }
 }
--- src/lesson02_variables/ReferenceTypesExample.java
+++ src/lesson02_variables/ReferenceTypesExample.java
@@ -1,4 +1,34 @@
 package lesson02_variables;
 
 public class ReferenceTypesExample {
+    public static void main(String[] args) {
+        //String (chuoi ky tu)
+        String myString = "Hello Java!";
+
+        //Array (mang)
+        int[] myArray = {1,2,3,4,5};
+
+        //Doi tuong (object)
+        Person person1 = new Person("Nguyen Van A", 20);
+
+        //In ra man hinh
+        System.out.println("String: " + myString);
+        System.out.print("Array: ");
+        for(int num : myArray){
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        System.out.println("Person Name: " + person1.name + ", Age: " + person1.age);
+    }
+}
+
+// Lop Person (de tao doi tuong)
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 }
